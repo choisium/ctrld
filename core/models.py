5c8c17671eaaed1d1026760ffed4a3925475a834
SoominChoi <choisium@gmail.com>
2019-04-27 13:36:17 +0900

    Make User model for convenience

--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+# 필요한 모델: User, Profile(if needed), Genre, Calendar, Event, Schedule
+
+class User(AbstractUser):
+    pass
